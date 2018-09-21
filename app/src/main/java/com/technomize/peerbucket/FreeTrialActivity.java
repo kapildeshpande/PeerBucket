d281253f1cbaf6881503f12d4e25b83f6e806a00
kapildeshpande <kapil.deshpande36@gmail.com>
2018-09-21 13:42:21 +0530

    Connected SignUpActivity with forgot_password_layout

--- app/src/main/java/com/technomize/peerbucket/FreeTrialActivity.java
+++ app/src/main/java/com/technomize/peerbucket/FreeTrialActivity.java
@@ -1,12 +1,15 @@
 package com.technomize.peerbucket;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 /**
@@ -18,6 +21,8 @@ public class FreeTrialActivity extends AppCompatActivity implements View.OnClick
     WebView mFreeTrialWebView;
     TextView mStartFreeTrial;
     ImageView mCloseFreeTrial;
+    ProgressBar progressBar;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,11 +31,27 @@ public class FreeTrialActivity extends AppCompatActivity implements View.OnClick
         mFreeTrialWebView=(WebView)findViewById(R.id.free_trial_webview);
         mStartFreeTrial=(TextView)findViewById(R.id.start_free_trial_tv);
         mCloseFreeTrial=(ImageView)findViewById(R.id.close_free_trial_iv);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
 
         mFreeTrialWebView.loadUrl("http://peerbuckets.tk/");
 
         mStartFreeTrial.setOnClickListener(this);
         mCloseFreeTrial.setOnClickListener(this);
+
+        mFreeTrialWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap facIcon) {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.GONE);
+            }
+
+        });
+
     }
 
     @Override
--- app/src/main/java/com/technomize/peerbucket/SignUpActivity.java
+++ app/src/main/java/com/technomize/peerbucket/SignUpActivity.java
@@ -1,19 +1,92 @@
 package com.technomize.peerbucket;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by rishi on 9/20/2018.
  */
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private LinearLayout forgotPasswordLayout,mainLayout;
+    private ImageView navigateBack;
+    private EditText email;
+    private Button navigateNext;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        forgotPasswordLayout = findViewById(R.id.forgot_password_layout);
+        mainLayout = findViewById(R.id.main_layout);
 
+        navigateBack = findViewById(R.id.navigate_back);
+
+        email = findViewById(R.id.email);
+        navigateNext = findViewById(R.id.navigate_next);
+
+        navigateNext.setOnClickListener(this);
+        navigateBack.setOnClickListener(this);
+    }
+
+    private static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+
+            case R.id.navigate_back:
+                if (forgotPasswordLayout.getVisibility() == View.VISIBLE) {
+                    forgotPasswordLayout.setVisibility(View.GONE);
+                    mainLayout.setVisibility(View.VISIBLE);
+                } else {
+                    finish();
+                }
+                break;
+
+            case R.id.navigate_next:
+                if (isEmailValid(email.getText().toString())) {
+                    InputMethodManager inputManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                    mainLayout.setVisibility(View.GONE);
+                    forgotPasswordLayout.setVisibility(View.VISIBLE);
+                } else {
+                    Toast.makeText(this,"Email is not valid", Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (forgotPasswordLayout.getVisibility() == View.VISIBLE) {
+            forgotPasswordLayout.setVisibility(View.GONE);
+            mainLayout.setVisibility(View.VISIBLE);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
