798616b9e1c332312cbaf309ced436c60767adec
ani2498 <aniketmodker24@gmail.com>
2018-09-21 14:07:05 +0530

    UI and classes added

--- app/src/main/java/com/technomize/peerbucket/SignUpActivity.java
+++ app/src/main/java/com/technomize/peerbucket/SignUpActivity.java
@@ -12,7 +12,7 @@ public class SignUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        setContentView(R.layout.activity_set_up_account);
 
 
     }
