74c85ee8e370e5ccbd542bcebbed3d21461443fe
ani2498 <aniketmodker24@gmail.com>
2018-09-28 12:27:17 +0530

    Home layout updated and dialog added with ui alignment changes

--- app/src/main/java/com/technomize/peerbucket/Activity/HomeActivity.java
+++ app/src/main/java/com/technomize/peerbucket/Activity/HomeActivity.java
@@ -1,27 +1,41 @@
 package com.technomize.peerbucket.Activity;
 
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.technomize.peerbucket.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
+    CardView mcv_company_hq, mcv_search_expand;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.temp);
+        setContentView(R.layout.activity_home);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mcv_company_hq=(CardView)findViewById(R.id.cv_company_hq);
+        mcv_search_expand=(CardView)findViewById(R.id.cv_search_expand);
+
         CollapsingToolbarLayout collapsingToolbar = findViewById(R.id.collapsing_toolbar);
         collapsingToolbar.setExpandedTitleTextAppearance(R.style.collapsingToolbarLayoutTitleColor);
         collapsingToolbar.setCollapsedTitleTextAppearance(R.style.collapsingToolbarLayoutTitleColor);
+
+
+
+        mcv_company_hq.setOnClickListener(this);
+        mcv_search_expand.setOnClickListener(this);
     }
 
     @Override
@@ -32,4 +46,31 @@ public class HomeActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.search:
+/*
+                mcv_search_expand.setVisibility(View.VISIBLE);
+*/
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.cv_company_hq:
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+
+                LayoutInflater inflater = this.getLayoutInflater();
+                View dialogView = inflater.inflate(R.layout.dialog_tcp_expand, null);
+                dialogBuilder.setView(dialogView);
+                final AlertDialog alertDialog = dialogBuilder.create();
+                alertDialog.show();
+                break;
+        }
+    }
 }
