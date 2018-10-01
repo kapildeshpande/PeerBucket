d885aad5998995c5ad35250e6c16f6c2015914fa
ani2498 <aniketmodker24@gmail.com>
2018-10-01 11:23:53 +0530

    Home layout updated and dialog added with ui alignment changes

--- app/src/main/java/com/technomize/peerbucket/Activity/HomeActivity.java
+++ app/src/main/java/com/technomize/peerbucket/Activity/HomeActivity.java
@@ -1,5 +1,7 @@
 package com.technomize.peerbucket.Activity;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -11,12 +13,18 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.technomize.peerbucket.R;
 
 public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
 
     CardView mcv_company_hq, mcv_search_expand;
+    LinearLayout mcreate_team_LL,mcreate_new_project_LL,mtab_start_hey_LL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +35,9 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
         mcv_company_hq=(CardView)findViewById(R.id.cv_company_hq);
         mcv_search_expand=(CardView)findViewById(R.id.cv_search_expand);
+        mcreate_team_LL=(LinearLayout)findViewById(R.id.create_team_LL);
+        mcreate_new_project_LL=(LinearLayout)findViewById(R.id.create_new_project_LL);
+        mtab_start_hey_LL=(LinearLayout)findViewById(R.id.tab_start_hey_LL);
 
         CollapsingToolbarLayout collapsingToolbar = findViewById(R.id.collapsing_toolbar);
         collapsingToolbar.setExpandedTitleTextAppearance(R.style.collapsingToolbarLayoutTitleColor);
@@ -36,6 +47,9 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
 
         mcv_company_hq.setOnClickListener(this);
         mcv_search_expand.setOnClickListener(this);
+        mcreate_team_LL.setOnClickListener(this);
+        mcreate_new_project_LL.setOnClickListener(this);
+        mtab_start_hey_LL.setOnClickListener(this);
     }
 
     @Override
@@ -70,6 +84,55 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
                 dialogBuilder.setView(dialogView);
                 final AlertDialog alertDialog = dialogBuilder.create();
                 alertDialog.show();
+                break;
+            case R.id.create_team_LL:
+                final Dialog dialog = new Dialog(this, android.R.style.Theme_Light);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+                dialog.setContentView(R.layout.dialog_create_tcp);
+                dialog.show();
+                TextView mheader_tcp_dialog_tv=(TextView) dialog.findViewById(R.id.header_tcp_dialog_tv);
+                TextView mtcp_name_header_tv=(TextView) dialog.findViewById(R.id.tcp_name_header_tv);
+                EditText mtcp_name_et=(EditText) dialog.findViewById(R.id.tcp_name_et);
+                TextView moptional_desc_tv=(TextView) dialog.findViewById(R.id.optional_desc_tv);
+                EditText moptional_desc_et=(EditText) dialog.findViewById(R.id.optional_desc_et);
+                mheader_tcp_dialog_tv.setText(R.string.tcp_dialog_header);
+                ImageView mclose_tcp_dialog_iv=(ImageView) dialog.findViewById(R.id.close_tcp_dialog_iv);
+                mclose_tcp_dialog_iv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                break;
+            case R.id.create_new_project_LL:
+                final Dialog dialog_np = new Dialog(this, android.R.style.Theme_Light);
+                dialog_np.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+                dialog_np.setContentView(R.layout.dialog_create_tcp);
+                dialog_np.show();
+                mheader_tcp_dialog_tv = (TextView) dialog_np.findViewById(R.id.header_tcp_dialog_tv);
+                mtcp_name_header_tv=(TextView) dialog_np.findViewById(R.id.tcp_name_header_tv);
+                mtcp_name_et=(EditText) dialog_np.findViewById(R.id.tcp_name_et);
+                moptional_desc_tv=(TextView) dialog_np.findViewById(R.id.optional_desc_tv);
+                moptional_desc_et=(EditText) dialog_np.findViewById(R.id.optional_desc_et);
+                moptional_desc_et.setHint(R.string.eg_optional_project_desc);
+                moptional_desc_tv.setText(R.string.optional_desc);
+                mtcp_name_et.setHint(R.string.eg_project_name);
+                mtcp_name_header_tv.setText(R.string.project_create_name);
+                mheader_tcp_dialog_tv.setText(R.string.tcp_project_dialog_header);
+                 mclose_tcp_dialog_iv=(ImageView) dialog_np.findViewById(R.id.close_tcp_dialog_iv);
+                mclose_tcp_dialog_iv.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog_np.dismiss();
+                    }
+                });
+                break;
+            case R.id.tab_start_hey_LL:
+                Intent startHeyActivity= new Intent(HomeActivity.this,HeyActivity.class);
+                startActivity(startHeyActivity);
+                finish();
                 break;
         }
     }
