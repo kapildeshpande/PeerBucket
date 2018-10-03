f026eefd66c007bd94413b66516d3cb402a69de3
ani2498 <aniketmodker24@gmail.com>
2018-10-03 17:50:38 +0530

    lollipop crash problem fixed

---
+++ app/src/main/java/com/technomize/peerbucket/Activity/CreateProjectActivity.java
@@ -0,0 +1,20 @@
+package com.technomize.peerbucket.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.technomize.peerbucket.R;
+
+/**
+ * Created by rishi on 10/3/2018.
+ */
+
+public class CreateProjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_tcp);
+    }
+}
