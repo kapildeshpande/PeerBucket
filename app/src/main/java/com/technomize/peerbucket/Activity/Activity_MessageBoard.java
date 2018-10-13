09066b3d3d9c2f707b21bc03269e5efa01901117
ani2498 <aniketmodker24@gmail.com>
2018-10-13 15:28:28 +0530

    13 oct

--- app/src/main/java/com/technomize/peerbucket/Activity/Activity_MessageBoard.java
+++ app/src/main/java/com/technomize/peerbucket/Activity/Activity_MessageBoard.java
@@ -12,8 +12,11 @@ import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.technomize.peerbucket.R;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by rishi on 10/10/2018.
@@ -21,9 +24,13 @@ import com.technomize.peerbucket.R;
 
 public class Activity_MessageBoard extends AppCompatActivity implements View.OnClickListener {
 
-    TextView mmessage_board_sort_end,mback_arrow_msg_box;
+    TextView mmessage_board_sort_end,mback_arrow_msg_box,mdialog_message_type_announcements,
+    mdialog_message_type_heartbeats,mdialog_message_type_pitches,mdialog_message_type_questions,
+    mdialog_message_type_fyis,mmessage_board_sort_type,mmessage_board_start,mdialog_message_type_all_messages_text,mdialog_message_type_all_announcements_text,
+    mdialog_message_type_all_heartbeats_text,mdialog_message_type_all_pitches_text,mdialog_message_type_all_questions_text,mdialog_message_type_all_fyis_text;
     Typeface fontTF;
-    LinearLayout mmessage_sort_LL;
+    LinearLayout mmessage_sort_LL,mdialog_message_type_all_messagesLL,mdialog_message_type_announcementsLL,mdialog_message_type_heartbeatsLL,
+    mdialog_message_type_pitchesLL,mdialog_message_type_questionsLL,mdialog_message_type_fyisLL;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,15 +42,20 @@ public class Activity_MessageBoard extends AppCompatActivity implements View.OnC
         mmessage_board_sort_end=(TextView) findViewById(R.id.message_board_sort_end);
         mback_arrow_msg_box=(TextView) toolbar.findViewById(R.id.back_arrow_chat_box);
         mmessage_sort_LL=(LinearLayout) findViewById(R.id.message_sort_LL);
+        mmessage_board_sort_type=(TextView) findViewById(R.id.messgae_board_sort_type);
+        mmessage_board_start=(TextView) findViewById(R.id.message_board_start);
 
 
         fontTF=Typeface.createFromAsset(getAssets(), "font/fawsmsolid.ttf");
 
         mmessage_board_sort_end.setTypeface(fontTF);
         mback_arrow_msg_box.setTypeface(fontTF);
+        mmessage_board_start.setTypeface(fontTF);
 
         mback_arrow_msg_box.setText("\uF060");
         mmessage_board_sort_end.setText("\uF0DC");
+
+        mmessage_sort_LL.setOnClickListener(this);
 
     }
 
@@ -54,14 +66,161 @@ public class Activity_MessageBoard extends AppCompatActivity implements View.OnC
                 AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
 
                 LayoutInflater inflater = this.getLayoutInflater();
-                View dialogView = inflater.inflate(R.layout.dialog_current_user, null);
+                View dialogView = inflater.inflate(R.layout.dialog_message_type, null);
                 dialogBuilder.setView(dialogView);
 
-                TextView edit_image=dialogView.findViewById(R.id.edit_image);
-                TextView timezoneTv=dialogView.findViewById(R.id.user_time_and_timezone);
-                AlertDialog alertDialog = dialogBuilder.create();
+                mdialog_message_type_announcements=(TextView) dialogView.findViewById(R.id.dialog_message_type_announcements);
+                mdialog_message_type_heartbeats=(TextView) dialogView.findViewById(R.id.dialog_message_type_heartbeats);
+                mdialog_message_type_pitches=(TextView) dialogView.findViewById(R.id.dialog_message_type_pitches);
+                mdialog_message_type_questions=(TextView) dialogView.findViewById(R.id.dialog_message_type_questions);
+                mdialog_message_type_fyis=(TextView) dialogView.findViewById(R.id.dialog_message_type_fyis);
+                mdialog_message_type_all_messagesLL=(LinearLayout) dialogView.findViewById(R.id.dialog_message_type_all_messagesLL); 
+                mdialog_message_type_announcementsLL= (LinearLayout) dialogView.findViewById(R.id.dialog_message_type_announcementsLL);
+                mdialog_message_type_heartbeatsLL=(LinearLayout) dialogView.findViewById(R.id.dialog_message_type_heartbeatsLL);
+                mdialog_message_type_pitchesLL=(LinearLayout)dialogView.findViewById(R.id.dialog_message_type_pitchesLL);
+                mdialog_message_type_questionsLL=(LinearLayout) dialogView.findViewById(R.id.dialog_message_type_questionsLL);
+                mdialog_message_type_fyisLL=(LinearLayout) dialogView.findViewById(R.id.dialog_message_type_fyisLL);
+
+                mdialog_message_type_all_messages_text=(TextView) dialogView.findViewById(R.id.dialog_message_type_all_messages_text);
+                mdialog_message_type_all_announcements_text=(TextView) dialogView.findViewById(R.id.dialog_message_type_all_announcements_text);
+                mdialog_message_type_all_heartbeats_text=(TextView) dialogView.findViewById(R.id.dialog_message_type_all_heartbeats_text);
+                mdialog_message_type_all_pitches_text=(TextView) dialogView.findViewById(R.id.dialog_message_type_all_pitches_text);
+                mdialog_message_type_all_questions_text=(TextView) dialogView.findViewById(R.id.dialog_message_type_all_questions_text);
+                mdialog_message_type_all_fyis_text=(TextView) dialogView.findViewById(R.id.dialog_message_type_all_fyis_text);
+
+                mdialog_message_type_announcements.setTypeface(fontTF);
+                mdialog_message_type_heartbeats.setTypeface(fontTF);
+                mdialog_message_type_pitches.setTypeface(fontTF);
+                mdialog_message_type_questions.setTypeface(fontTF);
+                mdialog_message_type_fyis.setTypeface(fontTF);
+
+                mdialog_message_type_announcements.setText("\uF0A1");
+                mdialog_message_type_heartbeats.setText("\uF004");
+                mdialog_message_type_pitches.setText("\uF0EB");
+                mdialog_message_type_questions.setText("\uF128");
+                mdialog_message_type_fyis.setText("\uF0D0");
+                
+                final AlertDialog alertDialog = dialogBuilder.create();
                 alertDialog.show();
+                
+                /*All Listeners here */
+                
+                mdialog_message_type_all_messagesLL.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mmessage_board_sort_type.setText(R.string.dialog_all_messsages);
+                        mmessage_board_start.setText("");
+                        alertDialog.dismiss();
+
+                    }
+                });
+                
+                mdialog_message_type_announcementsLL.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mmessage_board_sort_type.setText(R.string.dialog_announcements);
+                        mmessage_board_start.setText("\uF0A1");
+                        alertDialog.dismiss();
+
+                    }
+                });
+
+                mdialog_message_type_heartbeatsLL.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mmessage_board_sort_type.setText(R.string.dialog_heartbeats);
+                        mmessage_board_start.setText("\uF004");
+                        alertDialog.dismiss();
+
+
+                    }
+                });
+
+                mdialog_message_type_pitchesLL.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mmessage_board_sort_type.setText(R.string.dialog_pitches);
+                        mmessage_board_start.setText("\uF0EB");
+                        alertDialog.dismiss();
+
+                    }
+                });
+
+                mdialog_message_type_questionsLL.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mmessage_board_sort_type.setText(R.string.dialog_questions);
+                        mmessage_board_start.setText("\uF128");
+                        alertDialog.dismiss();
+
+
+                    }
+                });
+
+                mdialog_message_type_fyisLL.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        mmessage_board_sort_type.setText(R.string.dialog_FYI);
+                        mmessage_board_start.setText("\uF0D0");
+                        alertDialog.dismiss();
+
+                    }
+                });
+
+                /*Active text Check*/
+
+                if(mmessage_board_sort_type.getText().toString().equals(getString(R.string.dialog_all_messsages))){
+                    mdialog_message_type_all_messages_text.setTypeface(null,Typeface.BOLD);
+                    mdialog_message_type_all_announcements_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_heartbeats_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_pitches_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_questions_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_fyis_text.setTypeface(null,Typeface.NORMAL);
+                }
+                if(mmessage_board_sort_type.getText().toString().equals(getString(R.string.dialog_announcements))){
+                    mdialog_message_type_all_messages_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_announcements_text.setTypeface(null,Typeface.BOLD);
+                    mdialog_message_type_all_heartbeats_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_pitches_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_questions_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_fyis_text.setTypeface(null,Typeface.NORMAL);
+                }
+                if(mmessage_board_sort_type.getText().toString().equals(getString(R.string.dialog_heartbeats))){
+                    mdialog_message_type_all_messages_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_announcements_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_heartbeats_text.setTypeface(null,Typeface.BOLD);
+                    mdialog_message_type_all_pitches_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_questions_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_fyis_text.setTypeface(null,Typeface.NORMAL);
+
+                }
+                if(mmessage_board_sort_type.getText().toString().equals(getString(R.string.dialog_pitches))){
+                mdialog_message_type_all_messages_text.setTypeface(null,Typeface.NORMAL);
+                mdialog_message_type_all_announcements_text.setTypeface(null,Typeface.NORMAL);
+                mdialog_message_type_all_heartbeats_text.setTypeface(null,Typeface.NORMAL);
+                mdialog_message_type_all_pitches_text.setTypeface(null,Typeface.BOLD);
+                mdialog_message_type_all_questions_text.setTypeface(null,Typeface.NORMAL);
+                mdialog_message_type_all_fyis_text.setTypeface(null,Typeface.NORMAL);
+                }
+                if(mmessage_board_sort_type.getText().toString().equals(getString(R.string.dialog_questions))){
+                    mdialog_message_type_all_messages_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_announcements_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_heartbeats_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_pitches_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_questions_text.setTypeface(null,Typeface.BOLD);
+                    mdialog_message_type_all_fyis_text.setTypeface(null,Typeface.NORMAL);
+                }
+                if(mmessage_board_sort_type.getText().toString().equals(getString(R.string.dialog_FYI))){
+                    mdialog_message_type_all_messages_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_announcements_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_heartbeats_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_pitches_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_questions_text.setTypeface(null,Typeface.NORMAL);
+                    mdialog_message_type_all_fyis_text.setTypeface(null,Typeface.BOLD);
+                }
+
                 break;
+
         }
     }
 }
--- app/src/main/java/com/technomize/peerbucket/Activity/DocsAndFilesActivity.java
+++ app/src/main/java/com/technomize/peerbucket/Activity/DocsAndFilesActivity.java
@@ -1,15 +1,71 @@
 package com.technomize.peerbucket.Activity;
 
+import android.graphics.Typeface;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.technomize.peerbucket.R;
 
-public class DocsAndFilesActivity extends AppCompatActivity {
+public class DocsAndFilesActivity extends AppCompatActivity implements View.OnClickListener {
+
+    FloatingActionButton madd_docs_fab;
+    TextView mbs_docs_start,mbs_docs_folder,mbs_docs_upload,mbs_docs_gd,mbs_docs_dropbox,mbs_docs_box,
+            mbs_docs_onedrive;
+    Typeface typeface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_docs_and_files);
+
+        typeface=Typeface.createFromAsset(getAssets(), "font/fawsmsolid.ttf");
+
+        madd_docs_fab=(FloatingActionButton) findViewById(R.id.add_docs_fab);
+
+        madd_docs_fab.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.add_docs_fab:
+                View view = getLayoutInflater().inflate(R.layout.bottom_sheet_add_doc_and_files, null);
+
+                BottomSheetDialog dialog = new BottomSheetDialog(this);
+                dialog.setContentView(view);
+                dialog.show();
+
+
+                mbs_docs_start=(TextView) view.findViewById(R.id.bs_docs_start);
+                mbs_docs_folder=(TextView) view.findViewById(R.id.bs_docs_folder);
+                mbs_docs_upload=(TextView) view.findViewById(R.id.bs_docs_upload);
+                mbs_docs_gd=(TextView) view.findViewById(R.id.bs_docs_gd);
+                mbs_docs_dropbox=(TextView) view.findViewById(R.id.bs_docs_dropbox);
+                mbs_docs_box=(TextView) view.findViewById(R.id.bs_docs_box);
+                mbs_docs_onedrive=(TextView) view.findViewById(R.id.bs_docs_onedrive);
+
+                mbs_docs_start.setTypeface(typeface);
+                mbs_docs_folder.setTypeface(typeface);
+                mbs_docs_upload.setTypeface(typeface);
+                mbs_docs_gd.setTypeface(typeface);
+                mbs_docs_dropbox.setTypeface(typeface);
+                mbs_docs_box.setTypeface(typeface);
+                mbs_docs_onedrive.setTypeface(typeface);
+
+                mbs_docs_start.setText("\uF055");
+                mbs_docs_folder.setText("\uF055");
+                mbs_docs_upload.setText("\uF055");
+                mbs_docs_gd.setText("\uF055");
+                mbs_docs_dropbox.setText("\uF055");
+                mbs_docs_box.setText("\uF055");
+                mbs_docs_onedrive.setText("\uF055");
+
+                break;
+        }
     }
 }
